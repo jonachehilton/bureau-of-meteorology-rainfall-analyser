8663e5dc25fa10a6703ba813e84576b13631c894
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2020-10-20 22:33:46 +1000

    Add code to Loader class that processes raw csv file

--- src/rainfall/Loader.java
+++ src/rainfall/Loader.java
@@ -1,38 +1,69 @@
 package rainfall;
 
+import textio.TextIO;
+
 import java.io.File;
 import java.nio.file.Files;
+import java.util.HashMap;
 
 public class Loader {
 
     public static Station load(String directoryName, String stationName) throws LoaderException {
-        System.out.println("directoryName = " + directoryName);
-        System.out.println("stationName = " + stationName);
         String basePath = "" + directoryName + File.separator + stationName;
         String rawPath = basePath + ".csv";
         String fromPath = basePath + "_analysed.csv";
-        System.out.println("rawPath = " + rawPath);
-        System.out.println("fromPath = " + fromPath);
 
         File rawFile = new File(rawPath);
         File fromFile = new File(fromPath);
 
         if (Files.isRegularFile(rawFile.toPath())) {
             System.out.println("FOUND raw file = " + rawFile.toPath());
-        }
-        else {
+        } else {
             throw new LoaderException();
         }
+
         if (Files.isRegularFile(fromFile.toPath())) {
             System.out.println("FOUND _analysed file = " + fromFile.toPath());
-        }
-        else {
+        } else {
             System.out.println("MAKING _analysed file = " + fromFile.toPath());
-            // EVENTUALLY throw new LoaderException();
+            makeAndSave(rawPath, fromPath);
         }
-
-
         return null;
+    }
+
+
+    private static void makeAndSave(String rawPath, String dstPath) {
+
+
+        int count = -1;
+
+
+        while (!TextIO.eof()) {
+            count++;
+            if (count < 1) continue; // To prevent reading the column titles
+
+            String row = TextIO.getln();
+            String[] data = row.split(",");
+
+            int year = Integer.parseInt(data[2]);
+            if (year < 1000 || year > 2020) {
+                System.out.println("ERROR: An error occurred during runtime, failed to process file.");
+            }
+
+            int month = Integer.parseInt(data[3]);
+            if (month < 1 || month > 12) {
+                System.out.println("ERROR: An error occurred during runtime, failed to process file.");
+            }
+
+            int day = Integer.parseInt(data[4]);
+            if (day < 1 || day > 31) {
+                System.out.println("ERROR: An error occurred during runtime, failed to process file.");
+            }
+
+            double rain = data.length > 5 ? Double.parseDouble(data[5]) : 0;
+
+
+        }
     }
 
     public static class LoaderException extends Exception {
