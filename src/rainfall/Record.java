49d689bb45e9abe2b984d097004cdb14dab1fc13
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2020-10-22 21:51:34 +1000

    Add Record class method as well as getters and setters

--- src/rainfall/Record.java
+++ src/rainfall/Record.java
@@ -1,18 +1,58 @@
 package rainfall;
 
-/**
- * A Record contains the analysed rainfall data specific to a single month in a
- * specific year that comes from a particular rainfall station
- */
 public class Record {
     private int year;
     private int month;
-    private double totalMonthlyRainfall;
-    private double minimumDailyRainfallMonth;
-    private double minimumDailyRainfallYear;
-    private double maximumDailyRainfallMonth;
-    private double maximumDailyRainfallYear;
+    private double totalRain;
+    private double minRain;
+    private double maxRain;
+
+    public Record(int year, int month, double total, double min, double max) {
+        setYear(year);
+        setMonth(month);
+        setTotalRain(total);
+        setMinRain(min);
+        setMaxRain(max);
+    }
 
 
+    public void setYear(int year) {
+        this.year = year;
+    }
 
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setTotalRain(double total) {
+        this.totalRain = total;
+    }
+
+    public void setMinRain(double minRain) {
+        this.minRain = minRain;
+    }
+
+    public void setMaxRain(double maxRain) {
+        this.maxRain = maxRain;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public double getTotalRain() {
+        return totalRain;
+    }
+
+    public double getMinRain() {
+        return minRain;
+    }
+
+    public double getMaxRain() {
+        return maxRain;
+    }
 }
