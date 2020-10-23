f254031fc8f20995cb4b12d8b3f7300ad0401bd7
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2020-10-23 17:08:50 +1000

    Add button and clickOpenButton method to RainfallVisualiser

--- src/RainfallVisualiser.java
+++ src/RainfallVisualiser.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import rainfall.Loader;
 import rainfall.Station;
@@ -15,22 +18,32 @@ public class RainfallVisualiser extends Application {
     public void start(Stage stage) {
         var borderPane = new BorderPane();
         var scene = new Scene(borderPane);
-
+        borderPane.setPrefSize(1000,400);
         // setup window particulars and make it visible
         stage.setScene(scene);
         stage.setTitle("Rainfall Visualiser");
 
-        // add your UI control setup code here using helper methods
+        Button button = new Button("Open");
+        button.setOnAction( e -> clickOpenButton());
+
+        HBox hbox = new HBox(button);
+        hbox.setPadding(new Insets(10));
+        hbox.setSpacing(10);
+        borderPane.setTop(hbox);
 
         stage.show();
 
+}
+
+    private void clickOpenButton() {
         String stationName = "CopperlodeDamStation";
         String directoryName = "resources";
         try {
-        Station station = Loader.load(directoryName, stationName);
-    } catch (Loader.LoaderException e) {
+            station = Loader.load(directoryName, stationName);
+        } catch (Loader.LoaderException e) {
             e.printStackTrace();
-    }}
+        }
+    }
 
     public static void main(String[] args) {
         launch();
